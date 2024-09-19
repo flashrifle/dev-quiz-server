ea3355dba91990f30afa9468c9667918426fea78
flashrifle <flashrifle@gmail.com>
2024-09-19 16:43:13 +0900

    FEAT: CORS 설정

--- src/common/const/env-config.const.ts
+++ src/common/const/env-config.const.ts
@@ -1,2 +1,2 @@
-export const ENV_TEST = 'ENV_TEST';
+export const LOCAL_PORT = 'LOCAL_PORT';
 export const OPENAI_API_KEY = 'OPENAI_API_KEY';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
+
+const port = process.env['LOCAL_PORT'];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // cors 설정
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+  });
+
+  await app.listen(port);
+  console.log('server listen port :', port);
 }
 bootstrap();
