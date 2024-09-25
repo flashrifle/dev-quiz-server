04b007c79019294cff83154d121ca0aa2384ef56
flashrifle <flashrifle@gmail.com>
2024-09-25 18:04:18 +0900

    FEAT: DEV, PROD 환경에 따른 CORS HOST if 처리

--- src/common/const/env-config.const.ts
+++ src/common/const/env-config.const.ts
@@ -1,2 +1,5 @@
 export const LOCAL_PORT = 'LOCAL_PORT';
 export const OPENAI_API_KEY = 'OPENAI_API_KEY';
+export const DEV_HOST = 'DEV_HOST';
+export const PRODUCT_HOST = 'PRODUCT_HOST';
+export const DEV_STATUS = 'DEV_STATUS';
--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,26 @@ import { AppModule } from './app.module';
 import * as process from 'node:process';
 
 const port = process.env['LOCAL_PORT'];
-
+const devStatus = process.env['DEV_STATUS'];
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // cors 설정
-  app.enableCors({
-    origin: ['http://localhost:3000'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
-  });
+  if (devStatus === 'prod') {
+    app.enableCors({
+      origin: [process.env['PRODUCT_HOST']],
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+      exposedHeaders: ['Authorization'],
+    });
+  } else {
+    // cors 설정
+    app.enableCors({
+      origin: [process.env['DEV_HOST']],
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+      exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+    });
+  }
 
   await app.listen(port);
   console.log('server listen port :', port);
