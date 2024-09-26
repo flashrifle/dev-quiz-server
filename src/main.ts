053aeaedaffa91ed0b2a86984aa53d433f5d3916
flashrifle <flashrifle@gmail.com>
2024-09-26 16:22:17 +0900

    TEST: https TEST

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'node:process';
-import * as fs from 'fs';
 
 const port = process.env['LOCAL_PORT'];
 const devStatus = process.env['DEV_STATUS'];
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./localhost-key.pem'),
-    cert: fs.readFileSync('./localhost.pem'),
-  };
-
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  const app = await NestFactory.create(AppModule);
 
   if (devStatus === 'prod') {
     app.enableCors({
