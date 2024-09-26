be735903578b51e633194c801346d477e2a25c9f
flashrifle <flashrifle@gmail.com>
2024-09-26 23:30:23 +0900

    FEAT: prod mode 일경우 https 옵션 활성화

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'node:process';
+import * as fs from 'fs';
 
 const port = process.env['LOCAL_PORT'];
 const devStatus = process.env['DEV_STATUS'];
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let httpsOptions = undefined;
+
+  if (devStatus === 'prod') {
+    httpsOptions = {
+      key: fs.readFileSync('/etc/letsencrypt/live/devquiz.p-e.kr/privkey.pem'),
+      cert: fs.readFileSync(
+        '/etc/letsencrypt/live/devquiz.p-e.kr/fullchain.pem',
+      ),
+    };
+  }
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   if (devStatus === 'prod') {
     app.enableCors({
